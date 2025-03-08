9e2d25be5188b2f1f71cb5257c1160def8c81c0b
SanskrutiPadamatintiwar <147150465+SanskrutiPadamatintiwar@users.noreply.github.com>
2025-03-08 19:12:46 +0530

    Fixed some issues

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [
+//       "./index.html",
+//       "./src/**/*.{js,ts,jsx,tsx}",
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+
+// filepath: d:\RealtimeChatApp\tailwind.config.js
+module.exports = {
+  content: [
+    "./frontend/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
