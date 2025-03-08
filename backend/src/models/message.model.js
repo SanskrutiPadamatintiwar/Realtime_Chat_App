f117279bfcb676ede0e64273f45898c9d7a5cd6b
SanskrutiPadamatintiwar <147150465+SanskrutiPadamatintiwar@users.noreply.github.com>
2025-03-08 18:50:03 +0530

    tailwind setup for vite

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,27 +1,24 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({ 
     senderId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User", 
-        required:true,   
+        required: true,   
     },
-    receiverId:{
+    receiverId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User", 
-        required:true,   
+        required: true,   
     },
     text: {
         type: String,
-       
     },
-    image:{
-        type:String,
+    image: {
+        type: String,
     },
-},
-{timestamps:true}
-);
+}, { timestamps: true });
 
 const Message = mongoose.model("Message", messageSchema);
 
-export default Message;
+module.exports = Message;
